335f5152dd64bd773df4bffc89f699add3e85345
Marius <mariussangwa@gmail.com>
2026-01-30 16:02:34 +0200

    initial department schema

---
+++ models/department.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const departmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    company:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+    },
+
+    manager:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employee',
+        required: true,
+    },
+}, {timestamps: true});
+
+
+departmentSchema.index({name:1, company:1},{unique: true});
+
+module.exports = mongoose.model('Department',departmentSchema);
