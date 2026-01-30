358352bbf74f1ed7f98cbc5154419dffa7e029d4
Marius <mariussangwa@gmail.com>
2026-01-30 20:42:15 +0200

    adding the getAllProjects, updateProject, getProjectsByName and the delete controllers to projectControllers.js

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,19 +1,94 @@
 const  Project = require('../models/project');
 const mongoose = require('mongoose');
 
+const getAllProjects = async(req,res, next) =>{
+    try {
+        const projects = await Project.find()
+        .populate('company', 'name code email phone address')
+        .populate('manager',' names email phone  ')
+        .populate('members', 'names email phone');
+
+        if(!projects){
+            const error = new Error('No project added yet');
+            error.status = 404;
+            next(error);
+        }
+        res.status(200).json({
+            Total: projects.length,
+            message: "Here are the projects found",
+            projects
+        })
+    } catch (e) {
+        const error = new Error('Failed to get projects');
+        error.status = 500;
+        next(error);
+        console.log(e.message);
+        
+    }
+}
+
+
+
+const getProjectsByName = async (req,res,next)=>{
+    try {
+        const name = req.params.name;
+        if(!name){
+            const error = new Error('To get the project the name is required');
+            error.status = 400;
+            next(error);
+        }
+
+        const projects = await Project.find({name:{$regex:name, $options:'i'}})
+        .populate('company', 'name code email phone address')
+        .populate('manager',' names email phone  ')
+        .populate('members', 'names email phone');
+        if(!projects){
+            const error = new Error('No project found');
+            error.status = 404;
+            next(error);
+        }
+
+        res.status(200).json({
+            Total: projects.length,
+            message: "Here are the projects found",
+            projects
+        })
+    } catch (e) {
+        const error = new Error('Failed to get');
+        error.status = 500;
+        next(error);
+        console.log(e.message)
+        
+    }
+}
+
+
+
 const addProject = async (req ,res, next) =>{
     try {
         const { name, company, manager, members, status} = req.body;
-        if(!name ||!manager){
-            return res.status(400).json({message: "Project name and company are required"});
+        if(!name ||!company ||!manager){
+            const error = new Error('Project name, company id and manage id are required');
+            error.status = 400;
+            next(error);
         }
 
         if(!mongoose.Types.ObjectId.isValid(company)){
-            return res.status(400).json({message: "The company ID is invalid"});
+            const error = new Error('Company id is invalid');
+            error.status = 400;
+            next(error);
         }
 
         if(!mongoose.Types.ObjectId.isValid(manager)){
-            return res.status(400).json({message: "The manager ID is invalid"});
+            const error = new Error('Manager id is invalid');
+            error.status = 400;
+            next(error);
+        }
+
+        if(!mongoose.Types.ObjectId.isValid(members)){
+            const error = new Error('Members id is invalid');
+            error.status = 400;
+            next(error);
         }
 
         const project = await Project.create({
@@ -25,13 +100,107 @@ const addProject = async (req ,res, next) =>{
         })
 
         res.status(201).json({message: "Project added successfully",project});
-    } catch (error) {
-        res.status(500).json({message: "Can't add the Project"});
-        console.log(error.message)
+    } catch (e) {
+        const error = new Error('Failed to add Project');
+        error.status = 500;
+        next(error);
+        console.log(e.message)
     }
 }
 
 
+const updateProjectByname = async(req,res,next) =>{
+    try {
+        const name = req.params.name;
+        const {company, manager, members,status} = req.body;
+
+        if(!name){
+           const error = new Error('project name is required to update');
+           error.status = 400;
+           next(error);
+        }
+
+        if(!company ||!manager){
+            const error = new Error('Company id and manage id are required');
+            error.status = 400;
+            next(error);
+        }
+
+        if(!mongoose.Types.ObjectId.isValid(company)){
+            const error = new Error('Company id is invalid');
+            error.status = 400;
+            next(error);
+        }
+
+        if(!mongoose.Types.ObjectId.isValid(manager)){
+            const error = new Error('Manager id is invalid');
+            error.status = 400;
+            next(error);
+        }
+
+        if(!mongoose.Types.ObjectId.isValid(members)){
+            const error = new Error('Members id is invalid');
+            error.status = 400;
+            next(error);
+        }
+
+        const newProject = await Project.findOneAndUpdate(
+            {name},
+            {
+                company,
+                manager,
+                members,
+                status
+            }
+        );
+
+        if(!newProject){
+            const error = new Error(`No project called ${name}`);
+            error.status = 400;
+            next(error);
+        }
+
+        res.status(200).json({message: "Project updated successfully"});
+    } catch (e) {
+        const error = new Error(`Failed to add project ${name}`);
+        error.status = 500;
+        next(error);
+        console.log(e.message)
+    }
+
+}
+
+
+const deleteProjectByName = async (req,res,next) =>{
+    try {
+        const name = req.params.name;
+        if(!name){
+            const error = new Error('The name of the project should be provided');
+            error.status = 400;
+            next(error);
+        }
+
+        const project = await Project.findOne({name:name});
+        if(!project){
+            const error = new Error('Project not found');
+            error.status = 404;
+            next(error);
+        }
+
+        await Project.deleteOne({name:name});
+    } catch (e) {
+        const error = new Error('Failed to delete the project');
+        error.status = 500;
+        next(error)
+        console.log(e.message)
+        
+    }
+}
+
 module.exports = {
-    addProject
+    getAllProjects,
+    getProjectsByName,
+    addProject,
+    updateProjectByname,
+    deleteProjectByName
 };
