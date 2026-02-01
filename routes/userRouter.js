f83c428eaadbc3e4cf8807491848f63e5e197641
Marius <mariussangwa@gmail.com>
2026-02-01 21:34:23 +0200

    modified the userRoutes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const user = require('../controllers/authController')
+const auth = require('../middleware/auth');
+const validate = require('../middleware/validator');
+const regiseterSchema = require('../validations/userValidations')
 const router = express.Router();
-router.post('/register', user.register);
+router.post('/register',auth, validate(regiseterSchema), user.register);
 router.post('/login', user.login)
 
 module.exports = router
