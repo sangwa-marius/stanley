ec5b83a02494788bf5b1c277fdbfb7c92b0e997a
Marius <mariussangwa@gmail.com>
2026-02-01 18:21:44 +0200

    updated the company routes

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const company = require('../controllers/companyControllers')
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const validate = require('../middleware/validator');
+const {addCompanySchema,updateCompanyById} = require('../validations/companyValidations')
 
 router.get('/company',auth,company.getAllCompanies);
-router.get('/company/:name',company.getCompaniesByName);
-router.post('/company',company.addCompany);
-router.put('/company/:name',company.updateCompanyByName);
-router.delete('/company/:name',company.deletecompanyByName);
+router.get('/company/:name',auth,company.getCompaniesByName);
+router.post('/company',auth,validate(addCompanySchema),company.addCompany);
+router.put('/company/:id',auth,validate(updateCompanyById),company.updateCompanyById);
+router.delete('/company/:id',auth,company.deletecompanyById);
 
 module.exports = router;
