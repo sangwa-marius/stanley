f066fc0750e96faf9dd9b8cfbe7f21197dfff265
Marius <mariussangwa@gmail.com>
2026-05-07 20:37:34 +0200

    formatted the employee model

--- src/models/employees.ts
+++ src/models/employees.ts
@@ -2,45 +2,45 @@ import mongoose from 'mongoose';
 
 const employees = new mongoose.Schema({
 
-    names:{
-        type:String,
-        required:true,
+    names: {
+        type: String,
+        required: true,
         trim: true
 
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
     },
 
     phone: String,
 
-    department:{
-        type:mongoose.SchemaTypes.ObjectId,
-        ref:'Department',
-       
+    department: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Department',
+
     },
 
-    role:{
+    role: {
         type: mongoose.Schema.ObjectId,
         ref: 'Role',
     },
 
-    status:{
+    status: {
         type: String,
-        enum: ['ACTIVE','INACTIVE','SUSPENDED'],
+        enum: ['ACTIVE', 'INACTIVE', 'SUSPENDED'],
         default: 'ACTIVE'
     },
-    companies:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Company",
-        default:[]
+    companies: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+        default: []
     }],
 
-    hiredAt:Date,
-    
-}, {timestamps: true})
+    hiredAt: Date,
 
-export default mongoose.model('Employee',employees);
+}, { timestamps: true })
+
+export default mongoose.model('Employee', employees);
