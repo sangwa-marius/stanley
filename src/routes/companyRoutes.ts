560ec17f4067c6783fb541b6bc36f281d7b8f454
Marius <mariussangwa@gmail.com>
2026-07-02 04:15:09 +0200

    changed some routes to meet fronted implementation plan

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -5,11 +5,10 @@ import auth from '../middleware/auth';
 import validate from '../middleware/validator';
 import { addCompanySchema, updateCompanyByIdSchema } from '../validations/companyValidations';
 
-router.get('/get-all-companies',  company.getAllCompanies);
-router.get('/get-your-companies', company.getYourCompanies)
-router.get('get-company/:id', company.getCompany);
-router.post('/add-company',  validate(addCompanySchema), company.addCompany);
-router.put('/update-company/:id',  validate(updateCompanyByIdSchema), company.updateCompanyById);
-router.delete('/delete-company/:id',  company.deletecompanyById);
+router.get('/get-your-companies', auth, company.getYourCompanies)
+router.get('/get-company/:id', auth, company.getCompany);
+router.post('/add-company', auth, validate(addCompanySchema), company.addCompany);
+router.put('/update-company/:id', auth, validate(updateCompanyByIdSchema), company.updateCompanyById);
+router.delete('/delete-company/:id', auth, company.deletecompanyById);
 
 export default router;
